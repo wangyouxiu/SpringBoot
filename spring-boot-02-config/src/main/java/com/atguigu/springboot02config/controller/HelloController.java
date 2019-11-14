9e6502eb3b1a1358896de97176eeb1673be6e100
wangyue0302 <wangyue01@zto.cn>
2019-11-14 19:05:41 +0800

    10P springBoot config

---
+++ spring-boot-02-config/src/main/java/com/atguigu/springboot02config/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.atguigu.springboot02config.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0
+ * @ClassName HelloController
+ * @Description TODO描述
+ * @Author wangyue
+ * @Date 2019/11/14 18:52
+ **/
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World config";
+    }
+}
