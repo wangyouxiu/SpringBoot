da82f87717ce32b9ebb738eb27364e6e9d1a882c
wangyue <269773646@qq.com>
2019-11-17 11:07:28 +0800

    33P

---
+++ spring-boot-04-web-resfulcrud/src/main/java/com/atguigu/sporingboot04webresfulcrud/SpringBoot04WebResfulcrudApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.sporingboot04webresfulcrud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot04WebResfulcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot04WebResfulcrudApplication.class, args);
+    }
+
+}
---
+++ spring-boot-04-web-resfulcrud/src/test/java/com/atguigu/sporingboot04webresfulcrud/SporingBoot04WebResfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.sporingboot04webresfulcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SporingBoot04WebResfulcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
